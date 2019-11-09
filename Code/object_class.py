4ac0b3946f8d09b24e46458ee88514b0f4ed90d0
dakshp-fresh <dp.learning@posteo.net>
2019-11-09 12:11:11 -0700

    defined a few more functions for the TrivialObj class

--- Code/object_class.py
+++ Code/object_class.py
@@ -28,6 +28,50 @@ class TrivialObj:
 		self.latitude = 0
 		self.longitude = 0
 
+	def create_dict(self):
+		
+		obj_dict = {
+
+			"description" : self.description,
+			"alphanumeric_color" : self.alphanumeric_color,
+			"alphanumeric" : self.alphanumeric,
+			"shape" : self.shape,
+			"user" : self.user,
+			"background_color" : self.background_color,
+			"orientation" : self.alphanumeric_orientation,
+			"longitude" : self.longitude,
+			"latitude" : self.latitude,
+			"autonomous" : self.autonomous,
+			"type" : self.type
+
+		}
+
+		return obj_dict
+
+
+	def create_json(self, target_dir):
+		pass
+
+	def init_from_json(self, path_with_file):
+		
+		loaded_obj_dict = json.load(path_with_file)
+
+		self.description = loaded_obj_dict["description"]
+		self.user = loaded_obj_dict["user"]
+		self.autonomous = loaded_obj_dict["autonomous"]
+		self.type = loaded_obj_dict["type"]
+
+		self.alphanumeric_color = loaded_obj_dict["alphanumeric_color"]
+		self.alphanumeric = loaded_obj_dict["alphanumeric"]
+		self.shape = loaded_obj_dict["shape"]
+		self.shape_color = loaded_obj_dict["shape_color"]
+		self.alphanumeric_orientation = loaded_obj_dict["alphanumeric_orientation"]
+
+		self.latitude = loaded_obj_dict["latitude"]
+		self.longitude = loaded_obj_dict["longitude"]
+
+	def update_
+
 
 def main():
 	pass
